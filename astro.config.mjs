07792f5c3b436cd7f1aa6ac0ec37c43b2e3450eb
Kion <kion@dashgl.com>
2024-03-27 16:24:54 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kion-dgl.github.io',
+  base: '/daisy-app-shell',
   integrations: [tailwind()]
 });
