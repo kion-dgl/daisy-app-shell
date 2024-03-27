34d4bba7fa87d2f606af4a9c26683338538a760e
kion <kion@dashgl.com>
2024-03-27 12:30:31 +0900

    tailwind, typography and dasiy config

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui")
+	],
+	daisyui: {
+		themes: true,
+		base: true,
+		styled: true,
+		utils: true,
+		logs: true,
+		themeRoot: ":root",
+	},
+}
