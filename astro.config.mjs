6d26f502c453ba1c04a57c0a4453b7453cf5502d
kion <kion@dashgl.com>
2024-06-07 14:54:53 +0900

    add preact ui integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kion-dgl.github.io',
   base: '/daisy-app-shell',
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), preact()]
 });
