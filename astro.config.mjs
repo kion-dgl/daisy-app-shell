c0fbe0b1d5aa470f9482f50180b5ef6c8e836721
kion <kion@dashgl.com>
2024-06-08 14:44:27 +0900

    add solid integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kion-dgl.github.io',
   base: '/daisy-app-shell',
-  integrations: [tailwind(), db()]
+  integrations: [tailwind(), db(), solidJs()]
 });
