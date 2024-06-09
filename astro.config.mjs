1f048d2192ebe3c1354bc43f16d8e52fe26fdac2
kion <kion@dashgl.com>
2024-06-09 21:43:35 +0900

    add react, db, node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kion-dgl.github.io',
   base: '/daisy-app-shell',
-  integrations: [tailwind()]
+  integrations: [tailwind(), react(), db()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
